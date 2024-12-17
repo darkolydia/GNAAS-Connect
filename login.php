8f4d73325e304e885fb6d3215ceea5098f2d86a6
lydia.amoakoaa <lydia.amoakoaa@ashesi.edu.gh>
2024-12-17 20:41:25 +0000

    change

--- login.php
+++ login.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 include 'Lnavbar.php';
 include 'db_connection.php'; // Database connection
 
+session_start(); // Start the session
 
 $error_message = ''; // Error message for login failure
 
