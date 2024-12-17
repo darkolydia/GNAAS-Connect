3d75d23f7b735c5c3725e530682be8c021c7c43b
lydia.amoakoaa <lydia.amoakoaa@ashesi.edu.gh>
2024-12-17 19:54:10 +0000

    change

--- db_connection.php
+++ db_connection.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lydia.amoakoaa';
-$password = '2331jandec.';
+$password = '23Jan31Dec.';
 $dbname = 'webtech_fall2024_lydia_amoakoaa';
 
 $conn = new mysqli($host, $username, $password, $dbname);
