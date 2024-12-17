1c0413d5cce756db679941fe70310e564500bd68
lydia.amoakoaa <lydia.amoakoaa@ashesi.edu.gh>
2024-12-17 18:48:56 +0000

    change

--- signup.php
+++ signup.php
@@ -2,23 +2,6 @@
 
 echo "What at alll is wronggggg";
 
-<?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'agc2026';
-
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
-
 
 <?php
 session_start();
